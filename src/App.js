4c4b4a3da3b1f9f25cba2b649d8239df9914611e
moh1idris <113855948+moh1idris@users.noreply.github.com>
2022-10-15 11:39:09 +0530

    dynamic approach

--- src/App.js
+++ src/App.js
@@ -3,28 +3,35 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
+// const allCategories = new Set(items.map((item)=> item.category));                       ///--> Here Set is added to not to Repeat the Same CAtegory for the buttons 
+// console.log(allCategories);
+
+const allCategories = ['all', ...new Set(items.map((item) => item.category))];             ///--> Here <Set> is added to not to Repeat the Same CAtegory for the buttons --> here we are als0 using Spread Opertor even 
+console.log(allCategories);
+
+
 function App() {
   const [menuItems,setMenuItems] = useState(items);
-  const [categories,setCategories] = useState([]);
+  const [categories,setCategories] = useState(allCategories);
 
-  const filterItems = (category)=>{                                                    //--> making this to separate the category into items 
-    if(category === 'all'){                                                            //--> this will normally will scan the if cate==all then ist will display the all the items
+  const filterItems = (category)=>{                                                        //--> making this to separate the category into items 
+    if(category === 'all'){                                                                //--> this will normally will scan the if cate==all then ist will display the all the items
       setMenuItems(items);                
     return;
   }
-
     const newItems = items.filter((item)=> item.category === category);
     setMenuItems(newItems);
   }
+  
   return (
-  <main>u
+  <main>
     <section className="menu section">
       <div className="title">
         <h2>our menu</h2>
         <div className="underline"></div>
       </div>
-      <Categories filterItems={filterItems}/>
-      <Menu items={menuItems}/>                                            {/*Here items={menuItems}--> is nothing but Props*/}
+      <Categories filterItems={filterItems} categories={categories}/>
+      <Menu items={menuItems}/>                                                          {/*Here items={menuItems}--> is nothing but Props*/}
     </section>
 
   </main>);
--- src/Categories.js
+++ src/Categories.js
@@ -1,14 +1,24 @@
 import React from 'react';
 
-const Categories = ({ filterItems }) => {
+const Categories = ({ categories , filterItems }) => {
   return (
+    // <div className="btn-container">
+    //   {/* <button className="filter-btn" onClick={()=> filterItems('all')}>all</button>                          {/*Here im Manully adding the button as of now*/}
+    //   <button className="filter-btn" onClick={()=> filterItems('breakfast')}>breakfast</button>
+    //   <button className="filter-btn" onClick={()=> filterItems('lunch')}>lunch</button>
+    //   <button className="filter-btn" onClick={()=> filterItems('shakes')}>shakes</button> */}
+    // </div>
+
     <div className="btn-container">
-      <button className="filter-btn" onClick={()=> filterItems('all')}>all</button>                          {/*Here im Manully adding the button as of now*/}
-      <button className="filter-btn" onClick={()=> filterItems('breakfast')}>breakfast</button>
-      <button className="filter-btn" onClick={()=> filterItems('lunch')}>lunch</button>
-      <button className="filter-btn" onClick={()=> filterItems('shakes')}>shakes</button>
+      {categories.map((category, index) => {
+        return (
+          <button className="filter-btn" type='button' key={index} onClick={() => filterItems(category)}>
+            {category}
+          </button>
+        );
+      })}
     </div>
-  )
+  );
 };
 
 export default Categories;
